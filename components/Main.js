6c519b9723a736be8e599e4751b9a1392abdc291
Jack Latimer <jacklatimer03@gmail.com>
2021-03-16 02:23:36 +0000

    nine4-2

--- components/Main.js
+++ components/Main.js
@@ -2,7 +2,7 @@ export default function Main() {
   return (
     <section className="text-gray-600 body-font">
       <div className="max-w-7xl mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-        <div className="lg:flex-grow md:w-1/2 md:ml-24 ml-14 pt-6 flex flex-col md:items-start md:text-left mb-40 items-center text-center">
+        <div className="lg:flex-grow md:w-1/2 md:ml-24 pt-6 flex flex-col md:items-start md:text-left mb-40 items-center text-center">
           <h1 className="mb-5 sm:text-6xl text-5xl items-center Avenir xl:w-2/2 text-gray-900">
             We are making Stunning Websites
           </h1>
